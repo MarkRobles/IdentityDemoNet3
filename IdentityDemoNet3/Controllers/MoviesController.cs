3a986f01398cc1fd3012daa39c2bb4772b78151b
Marcos Robles <ecko_lob@outlook.com>
2021-03-28 18:56:00 -0600

    # Exportar a Excel *  Ejemplo super basico con Spire

--- IdentityDemoNet3/Controllers/MoviesController.cs
+++ IdentityDemoNet3/Controllers/MoviesController.cs
@@ -9,6 +9,11 @@ using IdentityDemoNet3;
 using IdentityDemoNet3.Models;
 using IdentityDemoNet3.IRepositories;
 using IdentityDemoNet3.ViewModels;
+using System.Data.OleDb;
+using System.Data;
+using Spire.Xls;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace IdentityDemoNet3.Controllers
 {
@@ -16,17 +21,58 @@ namespace IdentityDemoNet3.Controllers
     {
         private readonly IMovieRepositorie _movieRepositorie;
         private readonly IGenreRepositorie _genreRepositorie;
+        private readonly IConfiguration _configuration;
 
-        public MoviesController(IMovieRepositorie movieRepositorie,IGenreRepositorie genreRepositorie)
+        public MoviesController(IMovieRepositorie movieRepositorie,IGenreRepositorie genreRepositorie,IConfiguration  configuration)
         {
             
             _movieRepositorie = movieRepositorie;
             _genreRepositorie = genreRepositorie;
+            _configuration = configuration;
+        }
+
+
+        public async Task<IActionResult> Exportar(int? id) {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _movieRepositorie.GetById(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            //A: Dynamically create Excel file and save it to stream
+            Workbook wbToStream = new Workbook();
+            Worksheet sheet = wbToStream.Worksheets[0];
+            string fileName = string.Concat(movie.Description, ".xls");
+            sheet.Range["A1"].Text = "Titulo";
+            sheet.Range["A2"].Text = "Fecha de lanzamiento";
+            sheet.Range["A3"].Text = "Reseña";
+            sheet.Range["A4"].Text = "Precio";
+            sheet.Range["A5"].Text = "Genero";
+            sheet.Range["A6"].Text = "PreVenta";
+            sheet.Range["B1"].Text = movie.Title;
+            sheet.Range["B2"].Text = movie.ReleaseDate.ToShortDateString();
+            sheet.Range["B3"].Text = movie.Description;
+            sheet.Range["B4"].Text = movie.Price.ToString("C2");
+            sheet.Range["B5"].Text = movie.Genre.Name;
+            sheet.Range["B6"].Text = movie.Preorder==true?"SI":"NO";
+            FileStream file_stream = new FileStream(fileName, FileMode.Create);
+            wbToStream.SaveToStream(file_stream);
+            file_stream.Close();
+            //System.Diagnostics.Process.Start(fileName);
+
+            return RedirectToAction("Index");
+
         }
 
         // GET: Movies
         public async Task<IActionResult> Index()
         {
+
             return View(await _movieRepositorie.GetAll());
         }
 
