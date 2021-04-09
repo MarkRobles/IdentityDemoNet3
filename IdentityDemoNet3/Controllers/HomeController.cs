627be131b71073c9049f06d313f6858d5b7fe486
Marcos Robles <ecko_lob@outlook.com>
2021-04-08 21:55:27 -0500

    # Forgot password * No pude implementarlo porque perdi el hilo del curso pero quedo la idea y el ejemplo

--- IdentityDemoNet3/Controllers/HomeController.cs
+++ IdentityDemoNet3/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using IdentityDemoNet3.ViewModels;
 
 namespace IdentityDemoNet3.Controllers
 {
@@ -17,11 +18,14 @@ namespace IdentityDemoNet3.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> signInManager;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<Usuario> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<Usuario> userManager,
+            SignInManager<Usuario> signInManager)
         {
             _logger = logger;
             _userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -35,7 +39,8 @@ namespace IdentityDemoNet3.Controllers
         }
 
         [Authorize]
-        public IActionResult AcercaDe() {
+        public IActionResult AcercaDe()
+        {
             return View();
         }
 
@@ -74,28 +79,30 @@ namespace IdentityDemoNet3.Controllers
                     {
                         return View("Exito");
                     }
-                    else {
-                       
-                            var errorVM = new ErrorViewModel
-                            {
-                                Errores = result.Errors,
-                                RequestId = "1"
-                            };
-                            return View("Error", errorVM);
-                        
+                    else
+                    {
+
+                        var errorVM = new ErrorViewModel
+                        {
+                            Errores = result.Errors,
+                            RequestId = "1"
+                        };
+                        return View("Error", errorVM);
+
                     }
 
 
-                   
+
                 }
 
-              
+
             }
             return View();
         }
 
         [HttpGet]
-        public IActionResult IniciarSesion() {
+        public IActionResult IniciarSesion()
+        {
 
             return View();
         }
@@ -104,23 +111,91 @@ namespace IdentityDemoNet3.Controllers
         [HttpPost]
         public async Task<IActionResult> IniciarSesion(LoginViewModel model)
         {
-            if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
 
-                var user = await _userManager.FindByNameAsync(model.Descripcion);
-                if (user != null && await _userManager.CheckPasswordAsync(user,model.Contraseña)) {
-                  
-                    var identity = new ClaimsIdentity("cookies"); //This is the authority that issued this identity
-                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier,user.Id));
-                    identity.AddClaim(new Claim(ClaimTypes.Name,user.UserName));
 
-                    //Create a ClaimsPrincipal that represents the user
-                    await HttpContext.SignInAsync("cookies",new ClaimsPrincipal(identity));
-                  return   RedirectToAction("Index");
-
+                var signInResult = await signInManager.PasswordSignInAsync(model.Descripcion, model.Contraseña, false, false);
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
                 }
-            }
-            ModelState.AddModelError("","Usuario o contraseña invalidos");
+                //var user = await _userManager.FindByNameAsync(model.Descripcion);
+                //if (user != null && await _userManager.CheckPasswordAsync(user,model.Contraseña)) {
 
+                //    var identity = new ClaimsIdentity("cookies"); //This is the authority that issued this identity
+                //    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier,user.Id));
+                //    identity.AddClaim(new Claim(ClaimTypes.Name,user.UserName));
+
+                //    //Create a ClaimsPrincipal that represents the user
+                //    await HttpContext.SignInAsync("cookies",new ClaimsPrincipal(identity));
+                //  return   RedirectToAction("Index");
+
+                //}
+            }
+            ModelState.AddModelError("", "Usuario o contraseña invalidos");
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(vm.Correo);
+                if (user != null)
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var resetUrl = Url.Action("ResetPassword", "Home",
+                        new { token = token, correo = user.Email }, Request.Scheme);
+                    System.IO.File.WriteAllText("resetLink.txt", resetUrl);
+                }
+                else
+                {
+                    //email user and inform them that they do not have an account
+                }
+                return View("Exito");
+            }
+            return View();
+
+        }
+
+
+        [HttpGet]
+        public IActionResult ResetPassword(string token, string email)
+        {
+            return View(new ResetPasswordVM { Token = token, Correo = email });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordVM vm)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var user = await _userManager.FindByEmailAsync(vm.Correo);
+                if (user != null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user, vm.Token, vm.Contraseña);
+                    if (!result.Succeeded) {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("",error.Description);
+                        }
+                        return View();
+                    }
+                    return View("Exito");
+                }
+                ModelState.AddModelError("", "Solicitud Invalida");
+            }
             return View();
         }
 
---
+++ IdentityDemoNet3/ViewModels/ResetPasswordVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemoNet3.ViewModels
+{
+    public class ResetPasswordVM
+    {
+        public string Token { get; set; }
+        public string Correo { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Contraseña")]
+        public string ConfirmarContraseña { get; set; }
+    }
+}
